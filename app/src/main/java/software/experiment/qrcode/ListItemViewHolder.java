80574a54bf604b8a43e87ea416cdf8f9b41f6924
grf53 <grf53@naver.com>
2015-06-10 18:19:58 +0900

    UI Prototype

---
+++ app/src/main/java/software/experiment/qrcode/ListItemViewHolder.java
@@ -0,0 +1,9 @@
+package software.experiment.qrcode;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListItemViewHolder {
+	public ImageView bulletView;
+	public TextView titleView;
+}
